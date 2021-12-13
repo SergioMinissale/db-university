cad11bab8a4ed2a950af55de6e9901fdcd0fe269
Sergio Minissale <minissale.sergio@outlook.itm>
2021-12-13 17:50:31 +0100

    inizio parte dati

--- sql/schema.sql
+++ sql/schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE `corsi_di_laurea`(
 `id` INT NOT NULL AUTO_INCREMENT,
 `tipo_corso` VARCHAR(20)NOT NULL,
 `nome` VARCHAR(255) NOT NULL,
+`dipartimento_id` INT NOT NULL,
 PRIMARY KEY(`id`),
 FOREIGN KEY(`dipartimento_id`) REFERENCES dipartimento(`id`)
 );
@@ -22,6 +23,7 @@ CREATE TABLE `corsi_materie`(
 `id` INT NOT NULL AUTO_INCREMENT,
 `nome` VARCHAR(100) NOT NULL, 
 `durata_totale` INT,
+`corsi_di_laurea_id`INT NOT NULL,
 PRIMARY KEY(`id`),
 FOREIGN KEY(`corsi_di_laurea_id`) REFERENCES corsi_di_laurea(`id`)
 );
@@ -32,6 +34,7 @@ CREATE TABLE `studente`(
 `cognome` VARCHAR(100) NOT NULL,
 `matricola` CHAR(8) NOT NULL,
 `email` VARCHAR(100),
+`corsi_di_laurea_id`INT NOT NULL,
 PRIMARY KEY(`id`),
 FOREIGN KEY(`corsi_di_laurea_id`) REFERENCES corsi_di_laurea(`id`)
 );
@@ -40,12 +43,15 @@ CREATE TABLE `appello`(
 `id` INT NOT NULL AUTO_INCREMENT,
 `nome` VARCHAR(100) NOT NULL,
 `data` DATE NOT NULL,
+`corsi_materie_id` INT not null,
 PRIMARY KEY(`id`),
 foreign KEY(`corsi_materie_id`) REFERENCES corsi_materie(`id`) 
 );
 
 CREATE TABLE `voto`(
 `id` INT NOT NULL AUTO_INCREMENT,
+`studente_id` INT not null,
+`appello_id` INT not null,
 PRIMARY KEY(`id`),
 FOREIGN KEY(`studente_id`) REFERENCES studente(`id`),
 FOREIGN KEY(`appello_id`) REFERENCES appello(`id`),
@@ -56,7 +62,6 @@ CREATE TABLE `docente`(
 `nome` VARCHAR(100) NOT NULL,
 `cognome` VARCHAR(100) NOT NULL,
 `email` VARCHAR(100),
-PRIMARY KEY(`id`),
+PRIMARY KEY(`id`)
 );
-
 
